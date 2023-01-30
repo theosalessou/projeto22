69a427415725b4dda3507a75a697e4e3a80aa5e9
theosalessou <77073210+theosalessou@users.noreply.github.com>
2023-01-30 18:43:38 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,13 +16,7 @@ function preload(){
   jwelleryImg = loadImage("jwell.png");
   swordImg = loadImage("sword.png");
   gover = loadImage("gameOver.png");
-  //Remova os comentários corretos da linha de código para carregar a imagem gameOver.png
-  
-  gover=gameOver.png;
-  
-  gameOver.png=Load();
-  
-  gover = loadImage("gameOver.png");
+
 }
 
 function setup(){
@@ -95,18 +89,9 @@ function draw() {
         swordGroup.setVelocityYEach(0);
         //Remova os comentários corretos da linha de código para exibir 'game over' (fim de jogo)
 
-          over = createSprite(880,9980,20,20);
-          over.addAnimation("gameover",gover);
-          over.scale=0.7
-
           over = createSprite(180,280,20,20);
           over.addAnimation("gameover",gover);
           over.scale=0.7
-
-          over = createSprite(180,280,20,20);
-          over.addAnimation(gover);
-         over.scale=0.7
-  
     }
   }
   
